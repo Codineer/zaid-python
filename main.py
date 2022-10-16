86166bd8a43c3412e36e59b2b234e5839e86448a
Codineer <108866456+Codineer@users.noreply.github.com>
2022-10-16 12:55:33 -0400

    networkscaneer

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
-import subprocess
-import optparse
-import re
+import scapy.all as scapy
+
+# def get_arguments():
+#     import optparse
+#     parser = optparse.OptionParser()
+#     parser.add_option("-i", '--ipaddress', dest="ip_ad", help="write your victim's ip")
+#     options, arguments = parser.parse_args()
+#     return options
+
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option('-i', '--interface', dest="interface", help="change mac")
-    parser.add_option('-m', '--mac', dest="newmac", help="new mac option")
-    options, arguments = parser.parse_args()
-    if not options.interface:
-        parser.error("please specify the interface ")
-    elif not options.newmac:
-        parser.error("please specify the newmac ")
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", '--ipaddress', dest="ip_ad", help="write your victim's ip")
+    options = parser.parse_args()
     return options
 
-def change_mac(interface, new_mac):
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
-    print("mac address changing of "+interface)
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip) #specified the ip or ips to which we want to sent a arp request
+    broadcost = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")#used for getting an object ethernet frame which contains the destinmation mac and source mac
+    arp_request_broadcast = broadcost/arp_request #combining the packets to make a single packet
+    # print(arp_request_broadcast.psrc)
+    # print(arp_request_broadcast.hwsrc)
+    answered_list = scapy.srp(arp_request_broadcast , timeout=1,verbose=False)[0]#broadcast the packet and receives answered and unanswered packets.
+    answered_list_1=[]
+    for i in answered_list:#it contains more lists in the .each list in it consist of a broadcasted packet and a corresponding answered packet:
+        dic1=dict()
+        dic1['mac']=i[1].hwsrc
+        dic1['ip']=i[1].psrc
+        answered_list_1.append(dic1)
+    return answered_list_1
+    # print(answered_list.summary())#got the summary of all the answered ips in the subnet i.e we got there mac
+    # print(unanswered.summary())
+    # print(broadcost.summary())
+    # arp_request.show()
+    # broadcost.show()
+    # print(arp_request_broadcast.summary())
+    # arp_request_broadcast.show()#show() method gets more details of the packet
 
-def get_current_mac(interface):
-    ifconfig_result = str(subprocess.check_output(["ifconfig", interface]))
-    mac_address = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result)
-    if mac_address:
-        return mac_address.group(0)
-    else:
-        print("could'nt find mac ")
+    # scapy.ls(scapy.Ether())
+    # arp_request.pdst=ip
+    # print(arp_request.summary())
+    # scapy.ls(scapy.ARP()) used to list all the variables of the class specified
 
-options = get_arguments()
+def print_scan(answerlist):
+    print("IP\t\tMacaddress\n--------------------------------------------------------------------------")
+    for i in answerlist:
+        print(i['mac'],end="\t")
+        print(i['ip'])
 
-currentmac=get_current_mac(options.interface)
-print("current mac "+str(currentmac))
-change_mac(options.interface,options.newmac)
-currentmac=get_current_mac(options.interface)
-if currentmac==options.mac:
-    print("mac changed")
-else:
-    print("sorry")
-#subprocess.call("ifconfig "+interface+" down", shell=True)
-#subprocess.call("ifconfig "+interface+" hw ether " +address, shell=True)
-#subprocess.call("ifconfig "+interface+" up", shell=True)
-
+options=get_arguments()
+print_scan(scan(options.ip_ad))
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
